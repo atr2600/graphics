a09cd34824a0b34cb73208861e29111f6aa75845
Brandon Geraci <brand@DESKTOP-RQAVQ2V>
2019-02-27 21:12:26 -0600

    Added the autorun script

--- run.sh
+++ run.sh
@@ -6,4 +6,11 @@ cd main/
 
 make
 
-./run -w 250 -h 250 -i ../renderer/sceneData/scenes_A/spheres_1K.json -o ../../main/main.png
+./run -w 250 -h 250 -i ../renderer/sceneData/scenes_A/spheres_1K.json -o ../spheres_1k.png
+./run -w 250 -h 250 -i ../renderer/sceneData/scenes_A/lotsOTriangles.json -o ../lotsOTriangles.png
+./run -w 250 -h 250 -i ../renderer/sceneData/scenes_A/spheresAndTriangles.json -o ../spheresAndTriangles.png
+./run -w 250 -h 250 -i ../renderer/sceneData/scenes_A/oneTriangle.json -o ../oneTriangle.png
+./run -w 250 -h 250 -i ../renderer/sceneData/scenes_A/lambertianChalkSpheres.json -o ../lambertianChalkSpheres.png
+./run -w 250 -h 250 -i ../renderer/sceneData/scenes_A/PhongExp.json -o ../PhongExp.png
+./run -w 250 -h 250 -i ../renderer/sceneData/scenes_A/threeTriangles.json -o ../threeTriangles.png
+./run -w 250 -h 250 -i ../renderer/sceneData/scenes_A/oneSphere.json -o ../oneSphere.png
