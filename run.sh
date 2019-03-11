93632d0fcbde0aeabe8f0ec1fea45cd9575ec0c0
Brandon Geraci <bran0847@d.umn.edu>
2019-03-10 23:34:47 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,6 @@ cd main/
 make -j
 
 ./run -w 250 -h 250 -i ../renderer/sceneData/scenes_A/spheres_1K.json -o ../spheres_1k.png
-./run -w 250 -h 250 -i ../renderer/sceneData/scenes_A/lotsOTriangles.json -o ../lotsOTriangles.png
 ./run -w 250 -h 250 -i ../renderer/sceneData/scenes_A/oneTriangle.json -o ../oneTriangle.png
 ./run -w 250 -h 250 -i ../renderer/sceneData/scenes_A/lambertianChalkSpheres.json -o ../lambertianChalkSpheres.png
 ./run -w 250 -h 250 -i ../renderer/sceneData/scenes_A/PhongExp.json -o ../PhongExp.png
